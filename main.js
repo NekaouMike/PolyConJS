dd042ad4dba5fa676e336c7bdf1c88a75afcc61e
EmeraldMike <nekaoumike@gmail.com>
2023-04-01 19:54:25 -0400

    polyconjs@0.0.3

--- main.js
+++ main.js
@@ -49,6 +49,30 @@ record = async function(table,row,loc,data){
   });
 })
 }
+remove = async function(table,row,loc){
+  var ws = this.ws
+  var key = this.key
+  return new Promise(function (resolve, reject) {
+    var location = "rows"
+    if(row == "schema"){
+      location = "schema"
+    } else if(row == "all"){
+      location = "rows"
+    }
+    else{
+    if(loc == undefined){
+      location = "rows."+row
+    }else{
+    location = "rows."+row+"."+loc
+    }
+    }
+    ws.send(JSON.stringify({action: "record",password: key,dbname: table,location: location,value: null}))
+    ws.on('message', async function message(msg) {
+    var data = JSON.parse(msg)
+    resolve(data)
+  });
+})
+}
 getschema = async function(table){
   var ws = this.ws
   var key = this.key
@@ -81,12 +105,21 @@ retrieve = async function(table,row,loc){
   });
 })
 }
-search = async function(table,col,val){
+search = async function(table,col,val,index){
   var ws = this.ws
   var key = this.key
   return new Promise(function (resolve, reject) {
+    if(index == undefined){
+      index = true
+    }
     const query = col+":"+val
+    if(index == true){
+      ws.send(JSON.stringify({action: "index",password: key,dbname: table,location: "rows",value: query}))
+    }else if(index == false){
     ws.send(JSON.stringify({action: "search",password: key,dbname: table,location: "rows",value: query}))
+    }else{
+      reject("ERROR: Index must be true or false")
+    }
     ws.on('message', async function message(msg) {
     var data = JSON.parse(msg)
     resolve(data)
